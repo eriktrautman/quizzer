e1635305040d800606933d7c984d061cf9d05c56
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-10-12 17:58:43 -0700

    add associations to allow quiz.cards queries

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,5 +2,7 @@ class Quiz < ActiveRecord::Base
 
   belongs_to :user
   has_many :quiz_categories
+  has_many :categories, :through => :quiz_categories
+  has_many :cards, :through => :categories 
 
 end
