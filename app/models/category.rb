49e62cd55c3c0b58ad4777321e4b157860d33c6e
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-10-13 23:45:05 -0700

    add presence validation for name in categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,8 @@ class Category < ActiveRecord::Base
   has_many :quiz_categories, :dependent => :destroy
   has_many :user_category_queues, :dependent => :destroy
 
+  validates :name, :presence => true
+
   def new_queue
     self.cards.shuffle
   end
