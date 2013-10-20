ddaff7ee883d75207354bc4c0a0aa943dd507c75
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-10-20 16:55:46 -0700

    require the Card.rb in an initializer to circumvent deserializing issue that heroku can't handle but localhost can

---
+++ config/initializers/required_classes.rb
@@ -0,0 +1,6 @@
+  # This is necessary because when ActiveRecord tries to
+  # deserialize the "queue" column of UserCategoryQueue,
+  # it doesn't yet know what a "card" object is, and throws
+  # an error.  For more info, see:
+  # http://stackoverflow.com/questions/13750342/yamlload-raises-undefined-class-module-error
+  require 'Card'
