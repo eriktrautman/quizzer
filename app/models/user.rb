aeec0033634e323d44dae609f087327b5f5abd94
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-11-08 00:59:34 -0800

    debugging puts for cat q rebuilds again

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,17 +21,18 @@ class User < ActiveRecord::Base
   # for a given user and category, add any unused cards to the queue
   def rebuild_category_queue(category_id)
     category = Category.find(category_id)
-    puts "Rebuilding Category Queue for category: #{category.name}!"
+    puts "\n\nRebuilding Category Queue for category: #{category.name}!"
     ucq = self.user_category_queues.where(:category_id => category_id).first
-    puts "Old queue was #{ucq.queue}!"
+    puts "Old queue was #{ucq.queue.to_a}!"
     full_list = category.cards.to_a.map(&:id)
     puts "full list of card ids is #{full_list}!"
     # mash in all new cards but preserve order as much as possible
     # also ensure that any bogus cards that snuck in get weeded out
     # because apparently that's a problem somehow
     q = ucq.queue.to_a & full_list | full_list
+    puts "q is now #{q}!"
     ucq.update_attributes!(:queue => q)
-    puts "New queue is #{ucq.queue}!"
+    puts "New queue is #{ucq.queue}!\n\n"
   end
 
   # Update the pass numbers
